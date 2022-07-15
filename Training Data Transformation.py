b26a279395e05a9ba8f60b7171b1abf83839990b
sevchang <91085294+sevchang@users.noreply.github.com>
2022-07-16 01:54:19 +0800

    Update Training Data Transformation.py

--- Training Data Transformation.py
+++ Training Data Transformation.py
@@ -1,18 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
 import numpy as np
 import librosa
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MultiLabelBinarizer
-
-
-# In[2]:
 
 
 # load the resampled data
@@ -27,31 +18,21 @@ df = pd.read_pickle('gmd_100sample.pkl')
 df[['audio_wav_resample', 'resample_sr', 'label']].head()
 
 
-# In[3]:
-
-
 # label encoding            
 # some instances there are multiple positive labels
 
 mlb = MultiLabelBinarizer()
 y = pd.DataFrame(mlb.fit_transform(df['label']),columns=mlb.classes_)
 y
-
-
-# In[4]:
-
-
-# in the groove set there are 31 different labels
-
-
-# In[5]:
+# this is the original, complete labels 
+# in the gmd set there are 31 different labels
 
 
 # select only the the most common labels to model for now  
 # if we can find some solution(s) that work out well        then we expand the labels and data
 
-cols = ['D','S','H','_','e','o','c','s','l','K','C']
-label = y[cols]
+cols_of_interest = ['D','S','H','_','e','o','c','s','l','K','C']
+label = y[cols_of_interest]
 
 # since we leave out some labels     certain instances will have "no label" no
 # we encode these as "other"
@@ -63,7 +44,7 @@ label = y[cols]
 
 # this will be tested and we just take what turns out to work best since each way have it's own logic
 
-label['sum'] = label[cols].sum(axis = 1)
+label['sum'] = label[cols_of_interest].sum(axis = 1)
 
 def other_label(label_sum):
     if label_sum == 0:
@@ -73,19 +54,6 @@ def other_label(label_sum):
 
 label['other'] = label.apply(lambda row: other_label(row['sum']),axis=1)
 label.drop(columns=['sum'],inplace=True)
-
-label.head()
-
-
-# In[6]:
-
-
-# about 13.6% of the instances in the gmd set are labeled as "other",
-# namely, non of the top 11 labels appeared in these instances
-
-
-# In[7]:
-
 
 # turn the resampled autio_wav into the representation of short-time fourier transformation (stft)
 
@@ -101,17 +69,11 @@ X1.shape
 # this is in the format of 
 # a 4d array of (number of instances, y axis shape, x axis shape, 1 channel)
 
-
-# In[ ]:
-
-
 # 100 stft representation of 100 instances, each has the shape (1025,11), has 1 channel
 
 # (normal image classification tasks the instances will have multiple channels)
 # the task we're doing is audio classification, which will only have 1 channel for all representation formats
 
-
-# In[8]:
 
 
 # turn the resampled autio_wav into the representation of Mel-Frequency Spectrogram 
@@ -128,9 +90,6 @@ X2 = X2.reshape(X2.shape[0],X2.shape[1],X2.shape[2],1)
 X2.shape
 
 
-# In[9]:
-
-
 # turn the resampled autio_wav into the representation of Mel-Frequency Cepstral Coefficients
 
 mfcc_train = []
@@ -142,15 +101,10 @@ for i in range(df.shape[0]):
 
 X3 = np.array(mfcc_train)
 X3 = X3.reshape(X3.shape[0],X3.shape[1],X3.shape[2],1)
-
 X3.shape
-
-
-# In[10]:
 
 
 y = np.array(label)
 x_train, x_test, y_train, y_test = train_test_split(X1, y, test_size=0.2, random_state=42)
 
 x_train.shape, y_train.shape
-
