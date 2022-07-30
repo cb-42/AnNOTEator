6bc6481dc32a75b28dadb6854ae01e37a297f0c9
stanleyhung-hk <74230473+stanleyhung-hk@users.noreply.github.com>
2022-07-31 05:03:59 +0800

    add demucs support

--- input_transform.py
+++ input_transform.py
@@ -4,11 +4,15 @@ import librosa
 import pandas as pd
 from pytube import YouTube
 import warnings
+import numpy as np
+from demucs import pretrained, apply, audio
+from pathlib import Path
 
-def drum_extraction(path, music_start=None, music_end=None):
+def drum_extraction(path, kernel, drum_start=None, drum_end=None):
     """
     This is a function to transform the input audio file into a ready-dataframe for prediction task  
     :param path (str): the path to the audio file
+    :param kernel (str): 'spleeter' or 'demucs'. spleeter run faster but lower quality, demcus run slower but higher quality.  
     :param music_start (int): the start of the music in the file (in seconds). If not set, assume to start at the begining of the track
     :param music_end (int): the end of the music in the file (in seconds). If not set, assume to end at the end of the track
 
@@ -16,34 +20,75 @@ def drum_extraction(path, music_start=None, music_end=None):
     :return sample_rate (int): the sampling rate of the extracted drum track
     """
 
-    
-    #default to use 4stems pre-train model from the Spleeter package for audio demixing 
-    separator = Separator('spleeter:4stems')
-
-    audio_adapter = AudioAdapter.default()
-    #extract sampling rate from the audio file using the librosa package 
-    
-    if music_start!= None or music_end!=None:
-        if isinstance(music_start, type(None)):
+    if drum_start!= None or drum_end!=None:
+        if isinstance(drum_start, type(None)):
             raise ValueError('Please specify the music start time (in seconds) of your file / Youtube link')
-        if isinstance(music_end, type(None)):
+        if isinstance(drum_end, type(None)):
             raise ValueError('Please specify the music end time (in seconds) of your file / Youtube link')
-        y, sr=librosa.load(path, offset=music_start, duration=music_end-music_start, sr=None)
-        sample_rate = sr
-        waveform, _ = audio_adapter.load(path, offset=music_start, duration=music_end-music_start, sample_rate=sample_rate)
-    else:
-        y, sr=librosa.load(path, sr=None)
-        sample_rate = sr
-        waveform, _ = audio_adapter.load(path, sample_rate=sample_rate)
-        
-    
-    prediction = separator.separate(waveform)
 
-    #use librosa onset_detection algorithm to extract drum hit
-    drum_track=librosa.to_mono(prediction["drums"].T)
+    if kernel=='spleeter':
+    #default to use 4stems pre-train model from the Spleeter package for audio demixing 
+        separator = Separator('spleeter:4stems')
+
+        audio_adapter = AudioAdapter.default()
+        #extract sampling rate from the audio file using the librosa package 
+        
+        y, sr=librosa.load(
+            path,
+            offset=drum_start if drum_start is not None else None,
+            duration=drum_end-drum_start if drum_end is not None else None,
+            sr=None
+            )
+
+        sample_rate = sr
+
+        waveform, _ = audio_adapter.load(
+            path,
+            offset=drum_start if drum_start is not None else None,
+            duration=drum_end-drum_start if drum_end is not None else None,
+            sample_rate=sample_rate
+            )
+        
+        prediction = separator.separate(waveform)
+
+        #use librosa onset_detection algorithm to extract drum hit
+        drum_track=librosa.to_mono(prediction["drums"].T)
+
+    elif kernel=='demucs':
+        model_1=pretrained.get_model(name='14fc6a69', repo=Path('pretrained_models\demucs'))
+        model_2=pretrained.get_model(name='464b36d7', repo=Path('pretrained_models\demucs'))
+        model_3=pretrained.get_model(name='7fd6ef75', repo=Path('pretrained_models\demucs'))
+        model_4=pretrained.get_model(name='83fc094f', repo=Path('pretrained_models\demucs'))
+        model=apply.BagOfModels([model_1,model_2,model_3,model_4])
+
+        wav=audio.AudioFile(path).read(
+            streams=0,
+            samplerate=model.samplerate,
+            channels=model.audio_channels,
+            seek_time=drum_start if drum_start is not None else None,
+            duration=drum_end-drum_start if drum_end is not None else None
+            )
+
+        ref = wav.mean(0)
+        wav = (wav - ref.mean()) / ref.std()
+        sources = apply.apply_model(
+            model, wav[None],
+            device='cpu',
+            shifts=1,
+            split=True,
+            overlap=0.25,
+            progress=True,
+            num_workers=0
+            )[0]
+        
+        sources = sources * ref.std() + ref.mean()
+        drum=sources[0]
+        sample_rate=model.samplerate
+        drum_track=librosa.to_mono(drum)
+
     return drum_track, sample_rate
 
-def drum_to_frame(drum_track, sample_rate, estimated_bpm=None, resolution=8, fixed_clip_length=True, hop_length=1024):
+def drum_to_frame(drum_track, sample_rate, estimated_bpm=None, resolution=8, fixed_clip_length=True, hop_length=1024, backtrack=True):
 
     """
     This is a function to detect and extract onset from a drum track and format the onsets into a df for prediction task 
@@ -69,8 +114,11 @@ def drum_to_frame(drum_track, sample_rate, estimated_bpm=None, resolution=8, fix
         print('It is recommended to set the resolution value either 8 or 16, if not familiar with song structure')
         print('-----------------------------')
     
-    o_env = librosa.onset.onset_strength(drum_track, sr=44100, hop_length=hop_length)
-    onset_frames=librosa.onset.onset_detect(drum_track, onset_envelope=o_env, sr=sample_rate, backtrack=True)
+    if type(drum_track)!=np.ndarray:
+        drum_track, sample_rate=librosa.load(drum_track, sr=None)
+
+    o_env = librosa.onset.onset_strength(drum_track, sr=sample_rate, hop_length=hop_length)
+    onset_frames=librosa.onset.onset_detect(drum_track, onset_envelope=o_env, sr=sample_rate, backtrack=backtrack)
     peak_frames=librosa.onset.onset_detect(drum_track, onset_envelope=o_env, sr=sample_rate)
     onset_times=librosa.frames_to_time(onset_frames, sr=sample_rate, )
     onset_samples = librosa.frames_to_samples(onset_frames*(hop_length/512))
